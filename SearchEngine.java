fc2234095ea39f2abde16413a2700c935d745803
Eric Vu <56463764+air-wickvu@users.noreply.github.com>
2022-10-13 13:12:29 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,30 +1,23 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.*; 
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    ArrayList<String> items = new ArrayList <String>(); 
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
-            }
-            return "404 Not Found!";
+        System.out.println("Path: " + url.getPath());
+        if(url.getPath().contains("/add")){ 
+            String[] parameters = url.getQuery().split("=");
+            items.add(0,parameters[1]);
+            return parameters[1];
         }
-    }
+    return "404 Not Found!";
+    }   
 }
+
 
 class NumberServer {
     public static void main(String[] args) throws IOException {
